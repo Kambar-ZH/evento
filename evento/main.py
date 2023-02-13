816d50360927346c10159c08c4b7e122f6dd07e2
Kambar-ZH <zhamauov02@mail.ru>
2023-02-13 21:27:43 +0600

    Added auth logic

--- evento/main.py
+++ evento/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
-from .settings import settings
-from loguru import logger
+from fastapi.middleware.cors import CORSMiddleware
+from app.config import settings
+from app.routers import user, auth
 
 app = FastAPI()
 
+origins = [
+    settings.CLIENT_ORIGIN,
+]
 
-@app.get("/")
-def read_root():
-    logger.info(settings.docs_url)
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(auth.router, tags=['Auth'], prefix='/api/auth')
+app.include_router(user.router, tags=['Users'], prefix='/api/users')
+
+
+@app.get('/api/healthchecker')
+def root():
+    return {'message': 'Hello World'}
